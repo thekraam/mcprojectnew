48a85b0e0c0063d89a5905234bfc1ba90b49a131
ArVon27 <arcantos27@hotmail.it>
2022-03-09 18:04:14 +0100

    debugging e ricreazione di battle1.cs

--- RLikeProject/Assets/Scripts/game 2/battle 1.cs
+++ RLikeProject/Assets/Scripts/game 2/battle 1.cs
@@ -58,77 +58,154 @@ public class battle1 : MonoBehaviour
 
         float bonustot = bonusTerri + bonusSoldier + bonuscaptain;
         float bonusEtot = bonusETerri + bonusEnemy + bonusEcaptain;
-        //------------------------------------------------------------------------------------------------------
+        //---------------------------------------------variabili dell'algoritmo--------------------------------------------------
+        float ATK = 0;
+        float DEF = 0;
+        float EATK = 0;
+        float EDEF = 0;
 
-        while (turno < 4)
+        float percswordman = 0;
+        float percarcher = 0;
+        float percrider = 0;
+        float percEswordman = 0;
+        float percEarcher = 0;
+        float percErider = 0;
+
+        float x1;
+        float x2;
+        float y1;
+        float y2;
+        float z1;
+        float z2;
+
+        float totalsoldiers = 0;
+        float totalEsoldiers = 0;
+        float deadsoldier = 0;
+        float deadEsoldier = 0;
+        float deadswordman = 0;
+        float deadarcher = 0;
+        float deadrider = 0;
+        float deadeswordman = 0;
+        float deadearcher = 0;
+        float deaderider = 0;
+
+
+        while (turno < 6)
         {
             //----------------------------------calcoli iniziali del ciclo----------------------------------------
-            float ATK = (swordman.getAtk() * swordman.getTotal()) + (archer.getAtk() * archer.getTotal()) + (rider.getAtk() * rider.getTotal());
-            float DEF = (swordman.getDef() * swordman.getTotal()) + (archer.getDef() * archer.getTotal()) + (rider.getDef() * rider.getTotal());
-            float EATK = (eswordman.getAtk() * eswordman.getTotal()) + (earcher.getAtk() * earcher.getTotal()) + (erider.getAtk() * erider.getTotal());
-            float EDEF = (eswordman.getDef() * eswordman.getTotal()) + (earcher.getDef() * earcher.getTotal()) + (erider.getDef() * erider.getTotal());
-
-            float percswordman = (DEF / 100) * (swordman.getDef() * swordman.getTotal());
-            float percarcher = (DEF / 100) * (archer.getDef() * archer.getTotal());
-            float percrider = (DEF / 100) * (rider.getDef() * archer.getTotal());
-            float percEswordman = (DEF / 100) * (eswordman.getDef() * eswordman.getTotal());
-            float percEarcher = (DEF / 100) * (earcher.getDef() * earcher.getTotal());
-            float percErider = (DEF / 100) * (erider.getDef() * erider.getTotal());
+            ATK = (swordman.getAtk() * swordman.getTotal()) + (archer.getAtk() * archer.getTotal()) + (rider.getAtk() * rider.getTotal());
+            DEF = (swordman.getDef() * swordman.getTotal()) + (archer.getDef() * archer.getTotal()) + (rider.getDef() * rider.getTotal());
+            EATK = (eswordman.getAtk() * eswordman.getTotal()) + (earcher.getAtk() * earcher.getTotal()) + (erider.getAtk() * erider.getTotal());
+            EDEF = (eswordman.getDef() * eswordman.getTotal()) + (earcher.getDef() * earcher.getTotal()) + (erider.getDef() * erider.getTotal());
+            percswordman = (100 * (swordman.getDef() * swordman.getTotal())) / DEF;
+            percarcher = (100 * (archer.getDef() * archer.getTotal())) / DEF;
+            percrider = (100 * (rider.getDef() * rider.getTotal())) / DEF;
+            percEswordman = (100 * (eswordman.getDef() * eswordman.getTotal())) / EDEF;
+            percEarcher = (100 * (earcher.getDef() * earcher.getTotal())) / EDEF;
+            percErider = (100 * (erider.getDef() * erider.getTotal())) / EDEF;
             ATK = ATK + capitano1.getAtk();
             DEF = DEF + capitano1.getDef();
+            //Debug.LogError("EATK prova " + EATK);
+            // Debug.LogError("EDEF prova " + EDEF);
+            // Debug.LogError("Ecapitano ATK prova" + capitano2.getAtk());
+            //Debug.LogError("Ecapitano DEF prova" + capitano2.getDef());
             EATK = EATK + capitano2.getAtk();
             EDEF = EDEF + capitano2.getDef();
 
 
+            // Debug.LogError("EATK prova2 " + EATK);
+            // Debug.LogError("EDEF prova2 " + EDEF);
             ATK = ATK + ((ATK / 100) * bonustot);
             DEF = DEF + ((DEF / 100) * bonustot);
             EATK = EATK + ((EATK / 100) * bonusEtot);
             EDEF = EDEF + ((EDEF / 100) * bonusEtot);
 
             //-----------------------------passaggio 1 ===> x, y, z -----------------------------------------------------------
-            float x1 = 0;
-            float x2 = 0;
-            float y1 = 0;
-            float y2 = 0;
-            float z1 = 0;
-            float z2 = 0;
+            x1 = 0;
+            x2 = 0;
+            y1 = 0;
+            y2 = 0;
+            z1 = 0;
+            z2 = 0;
+            //Debug.LogError("ATK " + ATK);
+            //Debug.LogError("EDEF" + EDEF);
+            // Debug.LogError("EATK" + EATK);
+            // Debug.LogError("DEF" + DEF);
             x1 = ATK - EDEF;
-            rapporto1 = (int)(rapporto1 + x1);
+
             x2 = EATK - DEF;
-            rapporto2 = (int)(rapporto2 + x2);
-            y1 = EDEF / x1;
-            y2 = DEF / x2;
-            z1 = (int)Random.Range(1f, 15f);
+
+            y1 = (x1 * 100) / EDEF;
+            //  Debug.LogError("y1 " + y1);
+            y2 = (x2 * 100) / DEF;
+            // Debug.LogError("y2 " + y2);
+
+            z1 = (int)Random.Range(1f, 16f);
             z1 = z1 + ((z1 / 100) * y1);
-            z2 = (int)Random.Range(1f, 15f);
+            // Debug.LogError("z1 " + z1);
+            z2 = (int)Random.Range(1f, 16f);
             z2 = z2 + ((z2 / 100) * y2);
+            // Debug.LogError("z2 " + z2);
 
-
+            rapporto1 = (int)(rapporto1 + z1);
+            // Debug.LogError("rapporto 1 " + rapporto1);
+            rapporto2 = (int)(rapporto2 + z2);
+            // Debug.LogError("rapporto 2 " + rapporto2);
             //-----------------------------passaggio 2 ===>  morte soldati -----------------------------------------------------------
-            int totalsoldiers = swordman.getTotal() + archer.getTotal() + rider.getTotal();
-            int totalEsoldiers = eswordman.getTotal() + earcher.getTotal() + erider.getTotal();
+            totalsoldiers = swordman.getTotal() + archer.getTotal() + rider.getTotal();
+            totalEsoldiers = eswordman.getTotal() + earcher.getTotal() + erider.getTotal();
 
-            int deadsoldier = (int)((totalsoldiers / 100) * z1);
-            int deadEsoldier = (int)((totalEsoldiers / 100) * z2);
+            deadsoldier = (int)((totalsoldiers / 100) * z2);
+            //Debug.LogError("deadsoldier " + deadsoldier );
+            deadEsoldier = (int)((totalEsoldiers / 100) * z1);
+            // Debug.LogError("deadEsoldier " + deadEsoldier);
+            deadswordman = (int)((deadsoldier / 100) * percswordman);
+            //  Debug.LogError("deadswordman " + deadswordman);
+            deadarcher = (int)((deadsoldier / 100) * percarcher);
+            //   Debug.LogError("deadarcher " + deadarcher);
+            deadrider = (int)((deadsoldier / 100) * percrider);
+            //   Debug.LogError("deadrider " + deadrider);
+            deadeswordman = (int)((deadEsoldier / 100) * percEswordman);
+            //  Debug.LogError("deadEswordman " + deadeswordman);
+            deadearcher = (int)((deadEsoldier / 100) * percEarcher);
+            //  Debug.LogError("deadEarcher " + deadearcher);
+            deaderider = (int)((deadEsoldier / 100) * percErider);
+            //  Debug.LogError("deadErider " + deaderider);
 
-            int deadswordman = (int)((deadsoldier / 100) * percswordman);
-            int deadarcher = (int)((deadsoldier / 100) * percarcher);
-            int deadrider = (int)((deadsoldier / 100) * percrider);
-
-            int deadeswordman = (int)((deadEsoldier / 100) * percEswordman);
-            int deadearcher = (int)((deadEsoldier / 100) * percEarcher);
-            int deaderider = (int)((deadEsoldier / 100) * percErider);
-
-            swordman.setRapidTotal(-deadswordman);
-            archer.setRapidTotal(-deadarcher);
-            rider.setRapidTotal(-deadrider);
-            eswordman.setRapidTotal(-deadeswordman);
-            earcher.setRapidTotal(-deadearcher);
-            erider.setRapidTotal(-deaderider);
+            swordman.setRapidTotal((int)-deadswordman);
+            if (swordman.getTotal() < 0)
+            {
+                swordman.setRapidTotal(-swordman.getTotal());
+            }
+            archer.setRapidTotal((int)-deadarcher);
+            if (archer.getTotal() < 0)
+            {
+                archer.setRapidTotal(-archer.getTotal());
+            }
+            rider.setRapidTotal((int)-deadrider);
+            if (rider.getTotal() < 0)
+            {
+                rider.setRapidTotal(-rider.getTotal());
+            }
+            eswordman.setRapidTotal((int)-deadeswordman);
+            if (eswordman.getTotal() < 0)
+            {
+                eswordman.setRapidTotal(-eswordman.getTotal());
+            }
+            earcher.setRapidTotal((int)-deadearcher);
+            if (earcher.getTotal() < 0)
+            {
+                earcher.setRapidTotal(-earcher.getTotal());
+            }
+            erider.setRapidTotal((int)-deaderider);
+            if (erider.getTotal() < 0)
+            {
+                erider.setRapidTotal(-erider.getTotal());
+            }
 
             if (totalEsoldiers == 0 || totalsoldiers == 0)
             {
-                turno = 4;
+                turno = 6;
             }
 
 
@@ -137,12 +214,18 @@ public class battle1 : MonoBehaviour
 
 
 
-        int random1 = (int)Random.Range(1f, 5f);
-        int random2 = (int)Random.Range(1f, 5f);
-
-        rapporto1 = rapporto1 + ((rapporto1 / 100) * random1);
-        rapporto2 = rapporto2 + ((rapporto2 / 100) * random2);
-
+        int random1 = (int)Random.Range(1f, 10f);
+        int random2 = (int)Random.Range(1f, 10f);
+        float casorapporto = (((float)(rapporto1) / 100) * random1);
+        float casorapporto2 = (((float)(rapporto2) / 100) * random2);
+        // Debug.LogError("casorapporto1 " + casorapporto);
+        // Debug.LogError("casorapporto2" + casorapporto2);
+        // Debug.LogError("rapporto 1 quasi finale " + rapporto1);
+        // Debug.LogError("rapporto 2 quasi finale " + rapporto2);
+        rapporto1 = rapporto1 + (int)casorapporto;
+        rapporto2 = rapporto2 + (int)casorapporto2;
+        //  Debug.LogError("rapporto 1 finale " + rapporto1);
+        //  Debug.LogError("rapporto 2 finale " + rapporto2);
         if (rapporto1 >= rapporto2)
         {
             if (rapporto1 >= (rapporto2 + (rapporto2 / 2)))
@@ -165,7 +248,7 @@ public class battle1 : MonoBehaviour
                 fine = 2;
             }
         }
-
+        // Debug.LogError("numero vittoria " + fine);
         return fine;
     }
 
