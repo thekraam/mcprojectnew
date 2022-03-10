499f6216c73620f5d4421e1f9662bdab2cb54243
kraMonline <nesax2@hotmail.it>
2022-03-11 00:44:00 +0100

    Fix minori alla UI

--- RLikeProject/Assets/Scripts/Dialogue/DialogueManager.cs
+++ RLikeProject/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -88,7 +88,7 @@ public class DialogueManager : MonoBehaviour {
 		foreach (char letter in sentence.ToCharArray())
 		{
 			dialogueText.text += letter;
-			yield return null;
+			yield return new WaitForSeconds(0.02f);
 		}
 	}
 
