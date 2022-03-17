a5d8703bb6b0b2ff1dfcd53a19ec30c06b798642
kraMonline <nesax2@hotmail.it>
2022-03-17 19:37:58 +0100

    Fix Generali alla UI intro

--- RLikeProject/Assets/Scripts/game 1/IntroExecution.cs
+++ RLikeProject/Assets/Scripts/game 1/IntroExecution.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IntroExecution : MonoBehaviour
 {
@@ -12,9 +13,15 @@ public class IntroExecution : MonoBehaviour
     public CanvasGroup sixthLine;
     public CanvasGroup stars;
     public CanvasGroup IntroSequence;
+    public CanvasGroup Choice;
 
     bool skipIsPressed = false;
     bool skip = false;
+
+    public void SetLogo(Image logo)
+    {
+        
+    }
 
     public void StartExecution()
     {
@@ -163,7 +170,7 @@ public class IntroExecution : MonoBehaviour
                 firstLine.alpha = Mathf.Lerp(1f, 0f, timePassed3 / 0.8f);
                 secondLine.alpha = Mathf.Lerp(1f, 0f, timePassed3 / 0.8f); // da commentare qualora metodo 2
                 thirdLine.alpha = Mathf.Lerp(1f, 0f, timePassed3 / 0.8f); // da commentare qualora metodo 2
-                yield return new WaitForSeconds(0.025f);
+                yield return new WaitForSeconds(0.01f);
             }
         }
 
@@ -206,7 +213,7 @@ public class IntroExecution : MonoBehaviour
         }
         else
         {
-            yield return new WaitForSeconds(2.5f);
+            yield return new WaitForSeconds(3.25f);
         }
 
         while (timePassed4 < 0.8f) // fadeout fourthline
@@ -220,7 +227,7 @@ public class IntroExecution : MonoBehaviour
             {
                 timePassed4 += Time.deltaTime;
                 fourthLine.alpha = Mathf.Lerp(1f, 0f, timePassed4 / 0.8f);
-                yield return new WaitForSeconds(0.025f);
+                yield return new WaitForSeconds(0.01f);
             }
         }
 
@@ -231,7 +238,7 @@ public class IntroExecution : MonoBehaviour
         }
         else
         {
-            yield return new WaitForSeconds(2.5f);
+            yield return new WaitForSeconds(0.5f);
         }
 
         float timePassed5 = 0f;
@@ -247,7 +254,7 @@ public class IntroExecution : MonoBehaviour
             {
                 timePassed5 += Time.deltaTime;
                 fifthLine.alpha = Mathf.Lerp(0f, 1f, timePassed5 / 0.6f);
-                yield return new WaitForSeconds(0.025f);
+                yield return new WaitForSeconds(0.01f);
             }
         }
 
@@ -259,7 +266,7 @@ public class IntroExecution : MonoBehaviour
         }
         else
         {
-            yield return new WaitForSeconds(2.5f);
+            yield return new WaitForSeconds(3.2f);
         }
 
         while (timePassed5 < 0.6f) // fadeout fifthline
@@ -273,7 +280,7 @@ public class IntroExecution : MonoBehaviour
             {
                 timePassed5 += Time.deltaTime;
                 fifthLine.alpha = Mathf.Lerp(1f, 0f, timePassed5 / 0.6f);
-                yield return new WaitForSeconds(0.025f);
+                yield return new WaitForSeconds(0.01f);
             }
         }
 
@@ -284,7 +291,7 @@ public class IntroExecution : MonoBehaviour
         }
         else
         {
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(0.5f);
         }
 
         float timePassed6 = 0f;
@@ -299,7 +306,7 @@ public class IntroExecution : MonoBehaviour
             {
                 timePassed6 += Time.deltaTime;
                 sixthLine.alpha = Mathf.Lerp(0f, 1f, timePassed6 / 0.6f);
-                yield return new WaitForSeconds(0.025f);
+                yield return new WaitForSeconds(0.01f);
             }
         }
 
@@ -310,7 +317,7 @@ public class IntroExecution : MonoBehaviour
         }
         else
         {
-            yield return new WaitForSeconds(2.5f);
+            yield return new WaitForSeconds(3.2f);
         }
         timePassed6 = 0f;
 
@@ -322,35 +329,15 @@ public class IntroExecution : MonoBehaviour
             yield return new WaitForSeconds(0.025f);
         }
 
-        yield return new WaitForSeconds(3.2f);
+        yield return new WaitForSeconds(0.8f);
 
-        /* sparizione primo walltext */
-        /*while (timePassed4 < 0.8f)
+        float choiceTime = 0f;
+        while (choiceTime < 0.8f)
         {
-            timePassed4 += Time.deltaTime;
-            fourthLine.alpha = Mathf.Lerp(1f, 0f, timePassed4 / 0.8f);
-            //fifthLine.alpha = Mathf.Lerp(1f, 0f, timePassed4 / 0.8f); // da commentare qualora metodo 2
-            //sixthLine.alpha = Mathf.Lerp(1f, 0f, timePassed4 / 0.8f); // da commentare qualora metodo 2
-            yield return new WaitForSeconds(0.025f);
-        }*/
-
-        ///* metodo 2 *///
-
-        /* sparizione secondo walltext */
-
-        /*while (timePassed5 < 0.8f)
-        {
-            timePassed5 += Time.deltaTime;
-            fifthLine.alpha = Mathf.Lerp(1f, 0f, timePassed5 / 0.8f);
-            yield return new WaitForSeconds(0.025f);
-        }*/
-        /* sparizione terzo walltext */
-        /*while (timePassed6 < 0.8f)
-        {
-            timePassed6 += Time.deltaTime;
-            sixthLine.alpha = Mathf.Lerp(1f, 0f, timePassed6 / 0.8f);
-            yield return new WaitForSeconds(0.025f);
-        }*/
+            choiceTime += Time.deltaTime;
+            Choice.alpha = Mathf.Lerp(0f, 1f, choiceTime / 0.8f); // da 0f a 1f si accende, da 1f a 0f si spegne
+            yield return new WaitForSeconds(0.001f);
+        }
     }
 
 }
