3d80bb8fca344166b8c2f224a02a2be6733c6231
vla97 <vla.petriv@stud.uniroma3.it>
2022-07-31 15:59:25 +0200

    modificato il salvataggio

---
+++ RLikeProject/Assets/Scripts/DataGame/LoadGameData.cs
@@ -0,0 +1,206 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadGameData : MonoBehaviour
+{
+    public void LoadTutorial()
+    {
+        GameData data = SaveSystem.LoadGame();
+
+        /*tutorial*/
+        FindObjectOfType<Tutorial>().welcomeTutorial = data.welcomeTutorial;
+        FindObjectOfType<Game>().tutorialW = data.welcomeTutorial;
+        FindObjectOfType<Tutorial>().villageTutorial = data.villageTutorial;
+        FindObjectOfType<Tutorial>().farmTutorial = data.farmTutorial;
+        FindObjectOfType<Tutorial>().barracksTutorial = data.barracksTutorial;
+        FindObjectOfType<Tutorial>().blacksmithTutorial = data.blacksmithTutorial;
+        FindObjectOfType<Tutorial>().guildTutorial = data.guildTutorial;
+        FindObjectOfType<Tutorial>().mineTutorial = data.mineTutorial;
+        FindObjectOfType<Tutorial>().bonusTutorial = data.bonusTutorial;
+    }
+
+    public void loadgame(Player player, Fattoria fattoria, Miniera miniera, Caserma caserma, Enemy enemy, Fabbro fabbro, Gilda gilda)
+    {
+        GameData data = SaveSystem.LoadGame();
+
+        FindObjectOfType<Game>().CityNameUI.text = data.cityName;
+
+        player.player_turn = data.player_turn;
+        player.setPopulation(data.player_population);
+        player.player_citizens = data.player_citizens;
+        player.setCitizensMax(data.player_citizensMAX);
+        player.setCitizens();
+
+        player.temp_player_citizens = data.temp_player_citizens;
+        //player.setTempCitizens(data.temp_player_citizens);
+        player.player_money = data.player_money;
+
+        player.bonusBattle = data.bonusBattle;
+        player.bonusWall = data.bonusWall;
+        player.bonusCity = data.bonusCity;
+        player.bonusFar = data.bonusFar;
+        player.bonusDemoniac = data.bonusDemoniac;
+        player.lvlwall = data.lvlwall;
+        player.lvlfield = data.lvlfield;
+        player.soldiersaway = data.soldiersaway;
+
+        ///*tutorial*/
+        //FindObjectOfType<Tutorial>().welcomeTutorial = data.welcomeTutorial;
+        //tutorialW = data.welcomeTutorial;
+        //FindObjectOfType<Tutorial>().villageTutorial = data.villageTutorial;
+        //FindObjectOfType<Tutorial>().farmTutorial = data.farmTutorial;
+        //FindObjectOfType<Tutorial>().barracksTutorial = data.barracksTutorial;
+        //FindObjectOfType<Tutorial>().blacksmithTutorial = data.blacksmithTutorial;
+        //FindObjectOfType<Tutorial>().guildTutorial = data.guildTutorial;
+        //FindObjectOfType<Tutorial>().mineTutorial = data.mineTutorial;
+        //FindObjectOfType<Tutorial>().bonusTutorial = data.bonusTutorial;
+
+        /*events*/
+        FindObjectOfType<Events>().event1 = data.aqueduct;
+        FindObjectOfType<Events>().response[0] = data.response;
+        FindObjectOfType<Events>().secondaryEvent1MalusTurnsLeft = data.aqueductMalusTurnsLeft;
+        FindObjectOfType<Events>().secondaryEvent1Malus = data.aqueductMalus ? 1 : 0;
+        FindObjectOfType<Events>().event3 = data.citydefenseproject;
+        FindObjectOfType<Events>().secondaryEvent1 = data.aqueductSecondary;
+        FindObjectOfType<Events>().secondaryEvent1TurnsLeft = data.aqueductTurnsLeft;
+        FindObjectOfType<Events>().attendingSecondaryEvent = data.attendingSecondaryEvent;
+
+        /*farm*/
+        fattoria.setLvlFattoria(data.farmLvl);
+        fattoria.setAbitantiMax(data.abitantiMax);
+        fattoria.setCrescitaAbitanti(data.crescitaAbitanti);
+        fattoria.setGoldFattoria(data.farmGold);
+        fattoria.setLvlUpCost(data.farmLvlUpCost);
+        /*mine*/
+        miniera.lvl = data.minelvl;
+        miniera.gold = data.minegold;
+        miniera.carboneHigh = data.carboneHigh;
+        miniera.costo = data.minecosto;
+        /*barracks*/
+        caserma.lvl = data.casermalvl;
+        caserma.reclutamentoMAX = data.reclutamentoMAX;
+        caserma.bonusBarrack = data.bonusBarrack;
+        caserma.reclutamentoMaxMoment = data.reclutamentoMaxMoment;
+        caserma.costo = data.casermaLvlUpCost;
+
+        /*soldiers*/
+        /*swordsmen*/
+        FindObjectOfType<Game>().swordsmen.total_swordsmen = data.total_swordsmen;
+        FindObjectOfType<Game>().swordsmen.setTempTotal(data.temp_total_swordsmen);
+        FindObjectOfType<Game>().swordsmen.setTotal();
+        FindObjectOfType<Game>().swordsmen.atk_swordsmen = data.atk_swordsmen;
+        FindObjectOfType<Game>().swordsmen.temp_atk_swordsmen = data.temp_atk_swordsmen;
+        FindObjectOfType<Game>().swordsmen.def_swordsmen = data.def_swordsmen;
+        FindObjectOfType<Game>().swordsmen.temp_def_swordsmen = data.temp_def_swordsmen;
+        FindObjectOfType<Game>().swordsmen.bonus_swordsmen = data.bonus_swordsmen;
+        FindObjectOfType<Game>().swordsmen.momentswordman = data.momentswordman;
+        FindObjectOfType<Game>().swordsmen.momentDeadswordman = data.momentDeadswordman;
+        /*archers*/
+        FindObjectOfType<Game>().archers.total_archers = data.total_archers;
+        FindObjectOfType<Game>().archers.setTempTotal(data.temp_total_archers);
+        FindObjectOfType<Game>().archers.setTotal();
+        FindObjectOfType<Game>().archers.atk_archers = data.atk_archers;
+        FindObjectOfType<Game>().archers.temp_atk_archers = data.temp_atk_archers;
+        FindObjectOfType<Game>().archers.def_archers = data.def_archers;
+        FindObjectOfType<Game>().archers.temp_def_archers = data.temp_def_archers;
+        FindObjectOfType<Game>().archers.bonus_archers = data.bonus_archers;
+        FindObjectOfType<Game>().archers.momentarcher = data.momentarcher;
+        FindObjectOfType<Game>().archers.momentDeadArcher = data.momentDeadArcher;
+        /*riders*/
+        FindObjectOfType<Game>().riders.total_riders = data.total_riders;
+        FindObjectOfType<Game>().riders.setTempTotal(data.temp_total_riders);
+        FindObjectOfType<Game>().riders.setTotal();
+        FindObjectOfType<Game>().riders.atk_riders = data.atk_riders;
+        FindObjectOfType<Game>().riders.temp_atk_riders = data.temp_atk_riders;
+        FindObjectOfType<Game>().riders.def_riders = data.def_riders;
+        FindObjectOfType<Game>().riders.temp_def_riders = data.temp_def_riders;
+        FindObjectOfType<Game>().riders.bonus_riders = data.bonus_riders;
+        FindObjectOfType<Game>().riders.momentrider = data.momentrider;
+        FindObjectOfType<Game>().riders.momentDeadRider = data.momentDeadRider;
+
+        /*Enemy*/
+
+        enemy.totalSoldier = data.totalSoldierE;
+        enemy.lvl = data.lvlE;
+
+        FindObjectOfType<Game>().eswordsmen.total_swordsmen = data.total_swordsmenE;
+        FindObjectOfType<Game>().eswordsmen.atk_swordsmen = data.atk_swordsmenE;
+        FindObjectOfType<Game>().eswordsmen.def_swordsmen = data.def_swordsmenE;
+        FindObjectOfType<Game>().eswordsmen.bonus_swordsmen = data.bonus_swordsmenE;
+        FindObjectOfType<Game>().eswordsmen.deadEswordman = data.deadEswordmanE;
+
+        FindObjectOfType<Game>().earchers.total_archers = data.total_archersE;
+        FindObjectOfType<Game>().earchers.atk_archers = data.atk_archersE;
+        FindObjectOfType<Game>().earchers.def_archers = data.def_archersE;
+        FindObjectOfType<Game>().earchers.bonus_archers = data.bonus_archersE;
+        FindObjectOfType<Game>().earchers.deadEArchers = data.deadEArchersE;
+
+        FindObjectOfType<Game>().eriders.total_riders = data.total_ridersE;
+        FindObjectOfType<Game>().eriders.atk_riders = data.atk_ridersE;
+        FindObjectOfType<Game>().eriders.def_riders = data.def_ridersE;
+        FindObjectOfType<Game>().eriders.bonus_riders = data.bonus_ridersE;
+        FindObjectOfType<Game>().eriders.deadERiders = data.deadERidersE;
+
+        /*fabbro*/
+        fabbro.lvl = data.fablvl;
+        fabbro.costo = data.fabcosto;
+        fabbro.armi = data.armi;
+        fabbro.armature = data.armature;
+        fabbro.zappa = data.zappa;
+        fabbro.goldzappa = data.goldzappa;
+        fabbro.piccone = data.piccone;
+        fabbro.goldpiccone = data.goldpiccone;
+
+        /*gilda*/
+        gilda.lvl = data.gildalvl;
+        gilda.costo = data.gildacosto;
+        gilda.sped1 = data.sped1;
+        gilda.sped2 = data.sped2;
+        gilda.sped3 = data.sped3;
+        gilda.sped4 = data.sped4;
+        gilda.sped5 = data.sped5;
+        gilda.controllosped1 = data.controllosped1;
+        gilda.controllosped2 = data.controllosped2;
+
+        /*OldSoldiers*/
+        FindObjectOfType<OldSoldiersManager>().sword1 = data.sword1;
+        FindObjectOfType<OldSoldiersManager>().arc1 = data.arc1;
+        FindObjectOfType<OldSoldiersManager>().rid1 = data.rid1;
+        FindObjectOfType<OldSoldiersManager>().turn1 = data.turn1;
+
+        FindObjectOfType<OldSoldiersManager>().sword2 = data.sword2;
+        FindObjectOfType<OldSoldiersManager>().arc2 = data.arc2;
+        FindObjectOfType<OldSoldiersManager>().rid2 = data.rid2;
+        FindObjectOfType<OldSoldiersManager>().turn2 = data.turn2;
+
+        FindObjectOfType<OldSoldiersManager>().sword3 = data.sword3;
+        FindObjectOfType<OldSoldiersManager>().arc3 = data.arc3;
+        FindObjectOfType<OldSoldiersManager>().rid3 = data.rid3;
+        FindObjectOfType<OldSoldiersManager>().turn3 = data.turn3;
+
+        FindObjectOfType<OldSoldiersManager>().sword4 = data.sword4;
+        FindObjectOfType<OldSoldiersManager>().arc4 = data.arc4;
+        FindObjectOfType<OldSoldiersManager>().rid4 = data.rid4;
+        FindObjectOfType<OldSoldiersManager>().turn4 = data.turn4;
+
+        FindObjectOfType<OldSoldiersManager>().sword5 = data.sword5;
+        FindObjectOfType<OldSoldiersManager>().arc5 = data.arc5;
+        FindObjectOfType<OldSoldiersManager>().rid5 = data.rid5;
+        FindObjectOfType<OldSoldiersManager>().turn5 = data.turn5;
+
+        FindObjectOfType<OldSoldiersManager>().swordgilda1 = data.swordgilda1;
+        FindObjectOfType<OldSoldiersManager>().arcgilda1 = data.arcgilda1;
+        FindObjectOfType<OldSoldiersManager>().ridgilda1 = data.ridgilda1;
+        FindObjectOfType<OldSoldiersManager>().tipologia1 = data.tipologia1;
+        FindObjectOfType<OldSoldiersManager>().moltiplicatore1 = data.moltiplicatore1;
+        FindObjectOfType<OldSoldiersManager>().turngilda1 = data.turngilda1;
+
+        FindObjectOfType<OldSoldiersManager>().swordgilda2 = data.swordgilda2;
+        FindObjectOfType<OldSoldiersManager>().arcgilda2 = data.arcgilda2;
+        FindObjectOfType<OldSoldiersManager>().ridgilda2 = data.ridgilda2;
+        FindObjectOfType<OldSoldiersManager>().tipologia2 = data.tipologia2;
+        FindObjectOfType<OldSoldiersManager>().moltiplicatore2 = data.moltiplicatore2;
+        FindObjectOfType<OldSoldiersManager>().turngilda2 = data.turngilda2;
+    }
+}
--- RLikeProject/Assets/Scripts/game 1/Game.cs
+++ RLikeProject/Assets/Scripts/game 1/Game.cs
@@ -226,6 +226,7 @@ public class Game : MonoBehaviour
     Gilda gilda = new Gilda();
     //public static OldSoldiersManager manager = new OldSoldiersManager();
 
+    LoadGameData lgData = new LoadGameData(); 
 
     public List<Text> UIelements;
 
@@ -237,7 +238,7 @@ public class Game : MonoBehaviour
 
         // disattivo pannelli non di game, 'nse sa mai
         if (File.Exists(Application.persistentDataPath + "/game.fun"))
-            LoadTutorial();
+            lgData.LoadTutorial();
         mainMenuPanel.SetActive(true);
         skipTurnBlocker.SetActive(false);
         dialogueInterfaceBlocker.SetActive(false);
@@ -1089,10 +1090,7 @@ public class Game : MonoBehaviour
 
     }
 
-
-
-
-
+    /*------------------------------------------------*/
     /*----------------save------load------------------*/
 
     public void onTapForSave()
@@ -1114,211 +1112,12 @@ public class Game : MonoBehaviour
             );
     }
 
-    public void LoadTutorial()
+    public void loadData() // usata da unity dal pulsante resume game
     {
-        GameData data = SaveSystem.LoadGame();
-
-        /*tutorial*/
-        FindObjectOfType<Tutorial>().welcomeTutorial = data.welcomeTutorial;
-        tutorialW = data.welcomeTutorial;
-        FindObjectOfType<Tutorial>().villageTutorial = data.villageTutorial;
-        FindObjectOfType<Tutorial>().farmTutorial = data.farmTutorial;
-        FindObjectOfType<Tutorial>().barracksTutorial = data.barracksTutorial;
-        FindObjectOfType<Tutorial>().blacksmithTutorial = data.blacksmithTutorial;
-        FindObjectOfType<Tutorial>().guildTutorial = data.guildTutorial;
-        FindObjectOfType<Tutorial>().mineTutorial = data.mineTutorial;
-        FindObjectOfType<Tutorial>().bonusTutorial = data.bonusTutorial;
+        lgData.loadgame(player, fattoria, miniera, caserma, enemy, fabbro, gilda);
     }
 
-    public void LoadGame()
-    {
-        GameData data = SaveSystem.LoadGame();
-
-        CityNameUI.text = data.cityName;
-
-        player.player_turn = data.player_turn;
-        player.setPopulation(data.player_population);
-        player.player_citizens = data.player_citizens;
-        player.setCitizensMax(data.player_citizensMAX);
-        player.setCitizens();
-        
-        player.temp_player_citizens = data.temp_player_citizens;
-        //player.setTempCitizens(data.temp_player_citizens);
-        player.player_money = data.player_money;
-
-        player.bonusBattle = data.bonusBattle;
-        player.bonusWall = data.bonusWall;
-        player.bonusCity = data.bonusCity;
-        player.bonusFar = data.bonusFar;
-        player.bonusDemoniac = data.bonusDemoniac;
-        player.lvlwall = data.lvlwall;
-        player.lvlfield = data.lvlfield;
-        player.soldiersaway = data.soldiersaway;
-
-        ///*tutorial*/
-        //FindObjectOfType<Tutorial>().welcomeTutorial = data.welcomeTutorial;
-        //tutorialW = data.welcomeTutorial;
-        //FindObjectOfType<Tutorial>().villageTutorial = data.villageTutorial;
-        //FindObjectOfType<Tutorial>().farmTutorial = data.farmTutorial;
-        //FindObjectOfType<Tutorial>().barracksTutorial = data.barracksTutorial;
-        //FindObjectOfType<Tutorial>().blacksmithTutorial = data.blacksmithTutorial;
-        //FindObjectOfType<Tutorial>().guildTutorial = data.guildTutorial;
-        //FindObjectOfType<Tutorial>().mineTutorial = data.mineTutorial;
-        //FindObjectOfType<Tutorial>().bonusTutorial = data.bonusTutorial;
-
-        /*events*/
-        FindObjectOfType<Events>().event1 = data.aqueduct;
-        FindObjectOfType<Events>().response[0] = data.response;
-        FindObjectOfType<Events>().secondaryEvent1MalusTurnsLeft = data.aqueductMalusTurnsLeft;
-        FindObjectOfType<Events>().secondaryEvent1Malus = data.aqueductMalus ? 1 : 0;
-        FindObjectOfType<Events>().event3 = data.citydefenseproject;
-        FindObjectOfType<Events>().secondaryEvent1 = data.aqueductSecondary;
-        FindObjectOfType<Events>().secondaryEvent1TurnsLeft = data.aqueductTurnsLeft;
-        FindObjectOfType<Events>().attendingSecondaryEvent = data.attendingSecondaryEvent;
-
-        /*farm*/
-        fattoria.setLvlFattoria(data.farmLvl);
-        fattoria.setAbitantiMax(data.abitantiMax);
-        fattoria.setCrescitaAbitanti(data.crescitaAbitanti);
-        fattoria.setGoldFattoria(data.farmGold);
-        fattoria.setLvlUpCost(data.farmLvlUpCost);
-        /*mine*/
-        miniera.lvl = data.minelvl;
-        miniera.gold = data.minegold;
-        miniera.carboneHigh = data.carboneHigh;
-        miniera.costo = data.minecosto;
-        /*barracks*/
-        caserma.lvl = data.casermalvl;
-        caserma.reclutamentoMAX = data.reclutamentoMAX;
-        caserma.bonusBarrack = data.bonusBarrack;
-        caserma.reclutamentoMaxMoment = data.reclutamentoMaxMoment;
-        caserma.costo = data.casermaLvlUpCost;
-
-        /*soldiers*/
-        /*swordsmen*/
-        swordsmen.total_swordsmen = data.total_swordsmen;
-        swordsmen.setTempTotal(data.temp_total_swordsmen);
-        swordsmen.setTotal();
-        swordsmen.atk_swordsmen = data.atk_swordsmen;
-        swordsmen.temp_atk_swordsmen = data.temp_atk_swordsmen;
-        swordsmen.def_swordsmen = data.def_swordsmen;
-        swordsmen.temp_def_swordsmen = data.temp_def_swordsmen;
-        swordsmen.bonus_swordsmen = data.bonus_swordsmen;
-        swordsmen.momentswordman = data.momentswordman;
-        swordsmen.momentDeadswordman = data.momentDeadswordman;
-        /*archers*/
-        archers.total_archers = data.total_archers;
-        archers.setTempTotal(data.temp_total_archers);
-        archers.setTotal();
-        archers.atk_archers = data.atk_archers;
-        archers.temp_atk_archers = data.temp_atk_archers;
-        archers.def_archers = data.def_archers;
-        archers.temp_def_archers = data.temp_def_archers;
-        archers.bonus_archers = data.bonus_archers;
-        archers.momentarcher = data.momentarcher;
-        archers.momentDeadArcher = data.momentDeadArcher;
-        /*riders*/
-        riders.total_riders = data.total_riders;
-        riders.setTempTotal(data.temp_total_riders);
-        riders.setTotal();
-        riders.atk_riders = data.atk_riders;
-        riders.temp_atk_riders = data.temp_atk_riders;
-        riders.def_riders = data.def_riders;
-        riders.temp_def_riders = data.temp_def_riders;
-        riders.bonus_riders = data.bonus_riders;
-        riders.momentrider = data.momentrider;
-        riders.momentDeadRider = data.momentDeadRider;
-
-        /*Enemy*/
-
-        enemy.totalSoldier = data.totalSoldierE;
-        enemy.lvl = data.lvlE;
-
-        eswordsmen.total_swordsmen = data.total_swordsmenE;
-        eswordsmen.atk_swordsmen = data.atk_swordsmenE;
-        eswordsmen.def_swordsmen = data.def_swordsmenE;
-        eswordsmen.bonus_swordsmen = data.bonus_swordsmenE;
-        eswordsmen.deadEswordman = data.deadEswordmanE;
-
-        earchers.total_archers = data.total_archersE;
-        earchers.atk_archers = data.atk_archersE;
-        earchers.def_archers = data.def_archersE;
-        earchers.bonus_archers = data.bonus_archersE;
-        earchers.deadEArchers = data.deadEArchersE;
-
-        eriders.total_riders = data.total_ridersE;
-        eriders.atk_riders = data.atk_ridersE;
-        eriders.def_riders = data.def_ridersE;
-        eriders.bonus_riders = data.bonus_ridersE;
-        eriders.deadERiders = data.deadERidersE;
-
-        /*fabbro*/
-        fabbro.lvl = data.fablvl;
-        fabbro.costo = data.fabcosto;
-        fabbro.armi  = data.armi;
-        fabbro.armature = data.armature;
-        fabbro.zappa = data.zappa;
-        fabbro.goldzappa = data.goldzappa;
-        fabbro.piccone = data.piccone;
-        fabbro.goldpiccone = data.goldpiccone;
-
-        /*gilda*/
-        gilda.lvl = data.gildalvl;
-        gilda.costo = data.gildacosto;
-        gilda.sped1 = data.sped1;
-        gilda.sped2 = data.sped2;
-        gilda.sped3 = data.sped3;
-        gilda.sped4 = data.sped4;
-        gilda.sped5 = data.sped5;
-        gilda.controllosped1 = data.controllosped1;
-        gilda.controllosped2 = data.controllosped2;
-
-        /*OldSoldiers*/
-        FindObjectOfType<OldSoldiersManager>().sword1 = data.sword1;
-        FindObjectOfType<OldSoldiersManager>().arc1 = data.arc1;
-        FindObjectOfType<OldSoldiersManager>().rid1 = data.rid1;
-        FindObjectOfType<OldSoldiersManager>().turn1 = data.turn1;
-
-        FindObjectOfType<OldSoldiersManager>().sword2 = data.sword2;
-        FindObjectOfType<OldSoldiersManager>().arc2 = data.arc2;
-        FindObjectOfType<OldSoldiersManager>().rid2 = data.rid2;
-        FindObjectOfType<OldSoldiersManager>().turn2 = data.turn2;
-
-        FindObjectOfType<OldSoldiersManager>().sword3 = data.sword3;
-        FindObjectOfType<OldSoldiersManager>().arc3 = data.arc3;
-        FindObjectOfType<OldSoldiersManager>().rid3 = data.rid3;
-        FindObjectOfType<OldSoldiersManager>().turn3 = data.turn3;
-
-        FindObjectOfType<OldSoldiersManager>().sword4 = data.sword4;
-        FindObjectOfType<OldSoldiersManager>().arc4 = data.arc4;
-        FindObjectOfType<OldSoldiersManager>().rid4 = data.rid4;
-        FindObjectOfType<OldSoldiersManager>().turn4 = data.turn4;
-
-        FindObjectOfType<OldSoldiersManager>().sword5 = data.sword5;
-        FindObjectOfType<OldSoldiersManager>().arc5 = data.arc5;
-        FindObjectOfType<OldSoldiersManager>().rid5 = data.rid5;
-        FindObjectOfType<OldSoldiersManager>().turn5 = data.turn5;
-
-        FindObjectOfType<OldSoldiersManager>().swordgilda1 = data.swordgilda1;
-        FindObjectOfType<OldSoldiersManager>().arcgilda1 = data.arcgilda1;
-        FindObjectOfType<OldSoldiersManager>().ridgilda1 = data.ridgilda1;
-        FindObjectOfType<OldSoldiersManager>().tipologia1 = data.tipologia1;
-        FindObjectOfType<OldSoldiersManager>().moltiplicatore1 = data.moltiplicatore1;
-        FindObjectOfType<OldSoldiersManager>().turngilda1 = data.turngilda1;
-
-        FindObjectOfType<OldSoldiersManager>().swordgilda2 = data.swordgilda2;
-        FindObjectOfType<OldSoldiersManager>().arcgilda2 = data.arcgilda2;
-        FindObjectOfType<OldSoldiersManager>().ridgilda2 = data.ridgilda2;
-        FindObjectOfType<OldSoldiersManager>().tipologia2 = data.tipologia2;
-        FindObjectOfType<OldSoldiersManager>().moltiplicatore2 = data.moltiplicatore2;
-        FindObjectOfType<OldSoldiersManager>().turngilda2 = data.turngilda2;
-    }
-
-
-
-
-
-
+    
 
 
     //------------------------------------------ sistema di battaglia (beta testing)-----------------------
